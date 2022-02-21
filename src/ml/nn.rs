cc7cb0c39fa7dfa20b8f81e2b348856eea91d159
Sebastian Klähn <scoreplayer2000@gmail.com>
2022-02-21 17:14:04 +0100

    add total forward-pass for nn

--- src/ml/nn.rs
+++ src/ml/nn.rs
@@ -1,8 +1,8 @@
 #![allow(unused)]
 
-use std::f32::consts::E;
 use assert_approx_eq::assert_approx_eq;
-use nalgebra::{DMatrix, Matrix, SMatrix, SVector, Vector};
+use nalgebra::{DMatrix, Matrix, SMatrix, SVector, Vector, Vector2, Matrix2};
+use std::f32::consts::E;
 
 struct Layer<const INPUT: usize, const NODES: usize> {
     values: SVector<f32, NODES>,
@@ -22,18 +22,26 @@ impl<const INPUT: usize, const NODES: usize> Layer<INPUT, NODES> {
             applied_fn,
         }
     }
+    
+    fn from_weights(weights: SMatrix<f32, NODES, INPUT>, applied_fn: fn(&f32) -> f32) -> Self {
+        Self {
+            values: SVector::zeros(),
+            weights,
+            applied_fn,
+        }
+    }
 
     fn new(applied_fn: fn(&f32) -> f32) -> Self {
         Self {
-            values: SVector::new_random(),
+            values: SVector::zeros(),
             weights: SMatrix::new_random(),
             applied_fn,
         }
     }
 
-    fn forward(&mut self, input: SVector<f32, INPUT>) {
+    fn forward(&mut self, input: &SVector<f32, INPUT>) {
         let weight_x_input = self.weights * input;
-        self.values = weight_x_input.map(|element| (self.applied_fn)(&element));;
+        self.values = weight_x_input.map(|element| (self.applied_fn)(&element));
     }
 
     fn values(&self) -> &SVector<f32, NODES> {
@@ -42,16 +50,36 @@ impl<const INPUT: usize, const NODES: usize> Layer<INPUT, NODES> {
 }
 
 struct NN {
-    input: SVector<f32, 2>,
-    hidden: [SVector<f32, 2>; 2],
-    output: SVector<f32, 1>,
+    layers: (Layer<1, 2>, Layer<2, 2>, Layer<2, 1>),
+}
+
+fn vec2(x: f32, y: f32) -> Vector2<f32> {
+    [x, y].into()
 }
 
 impl NN {
-    fn forward(&mut self) {
-        //let neural_values = [];
-        for layer in self.hidden {}
+    fn new() -> Self {
+        Self {
+            layers: (
+                Layer::from_weights(vec2(1., -1.),relu),
+                Layer::from_weights(Matrix2::new(-1., 1., 1., -1.),relu),
+                Layer::from_weights(vec2(-1., 1.).transpose(),relu),
+            ),
+        }
     }
+
+    fn forward(&mut self, input: SVector<f32, 1>) -> f32 {
+        //let neural_values = [];
+
+        self.layers.0.forward(&input);
+        self.layers.1.forward(self.layers.0.values());
+        self.layers.2.forward(self.layers.1.values());
+        self.layers.2.values()[0]
+    }
+}
+
+fn relu(x: &f32) -> f32 {
+    if *x > 0. {*x} else {0.}
 }
 
 fn sigmoid(x: &f32) -> f32 {
@@ -72,8 +100,18 @@ mod tests {
             sigmoid,
         );
         let input: SVector<f32, 2> = [5., 1.].into();
-    
-        layer.forward(input);
-        assert_eq!(*layer.values(), <SVector<f32, 2>>::from([0.00669285236, 0.268941432]));
+
+        layer.forward(&input);
+        assert_eq!(
+            *layer.values(),
+            <SVector<f32, 2>>::from([0.00669285236, 0.268941432])
+        );
+    }
+
+    #[test]
+    fn test_nn_forward() {
+        let mut nn = NN::new();
+        let out = nn.forward([3.0].into());
+        assert_eq!(out, 3.0);
     }
 }
