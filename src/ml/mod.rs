222a1a049b900b209450fbdb3f3338d2e71ae444
Sebastian Klähn <scoreplayer2000@gmail.com>
2022-02-27 10:48:10 +0100

    changes in mod?

--- src/ml/mod.rs
+++ src/ml/mod.rs
@@ -1,7 +1,8 @@
 pub mod ada_boosting;
-pub mod regression_trees;
 pub mod helpers;
 pub mod linear_classification;
+pub mod linear_regression;
 pub mod logistic_regression;
 pub mod neuronal_networ;
-pub mod linear_regression;
+pub mod regression_trees;
+pub mod gbdt;
